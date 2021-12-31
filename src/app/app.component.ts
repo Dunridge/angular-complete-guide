a50bfe4ca87ce33c29063ae969c305205040209d
Dunridge <Polinka7max@gmail.com>
2021-12-31 11:23:20 +0200

    develop. removed test generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-complete-guide';
+  // TODO: figure out how to disable tests for all generated stuff for the app
 }
