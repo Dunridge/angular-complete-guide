ce27a88ff9ae0ff67af2c9f2686b101aada73b93
Dunridge <Polinka7max@gmail.com>
2022-01-16 10:58:00 +0200

    databinding. defined the base app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ServerComponent} from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import {SuccessAlertComponent} from "./success-alert/success-alert.component";
+import {ServersComponent} from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
