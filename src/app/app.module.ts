86d55b81657fe96f07e5ce6a912ff9c9bdc3b24d
Dunridge <Polinka7max@gmail.com>
2022-04-02 14:45:38 +0300

    components-app. added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,24 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
-import { ShoppingListEditComponent } from './components/shopping-list/components/shopping-list-edit/shopping-list-edit.component';
-import { IngredientComponent } from './components/shopping-list/components/ingredient/ingredient.component';
-import { RecipeListComponent } from './components/recipe-book/components/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './components/recipe-book/components/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './components/recipe-book/components/recipe-detail/recipe-detail.component';
+import {HeaderComponent} from './header/header.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import {RecipesComponent} from "./recipes/recipes.component";
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeBookComponent,
-    ShoppingListEditComponent,
-    IngredientComponent,
     RecipeListComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
