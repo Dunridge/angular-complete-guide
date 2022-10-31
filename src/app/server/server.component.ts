2363e871f3c9bab1bc1dd087e8e7942582a27902
Dunridge <Polinka7max@gmail.com>
2022-10-31 10:53:37 +0200

    conditional-display. added ngFor

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,30 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    p {
+      padding: 2px;
+    }
+
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
 
-  getServerStatus() {
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus(): string {
     return this.serverStatus;
+  }
+
+  getColor(): string {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
